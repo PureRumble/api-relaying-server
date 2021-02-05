be50a3614238860598716c2fd97361cbb3970d9e
Arash Bazrafshan <arashu@gmail.com>
2021-02-05 14:59:24 +0100

    Add .gitignore to ignore __pycache__ dirs

--- python_source_files/api_relaying_server/api_relaying_request_handler.py
+++ python_source_files/api_relaying_server/api_relaying_request_handler.py
@@ -6,6 +6,7 @@ import requests
 remote_server_url = "https://menu.dckube.scilifelab.se/api"
 
 class ApiRelayingRequestHandler(BaseHTTPRequestHandler):
+
     def do_GET(self):
         response = requests.get(remote_server_url + self.path, stream=True)
 
